d124bb9bcb5afa87c805282419d6343df77da739
Aarushi Johly <aarushi.johly@gmail.com>
2024-12-24 19:08:29 +0530

    changes made

--- arrays.py
+++ arrays.py
@@ -2,27 +2,33 @@
 import ctypes
 
 class DynamicArray():
+    """A dynamic array class akin to a simplified Python list."""
 
     def __init__(self):
+        """Create an empty array."""
         self._n = 0
         self._capacity = 1;
         self._A = self._make_array(self._capacity)
 
     def __len__(self):
+        """Return number of elements stored in the array."""
         return self._n
 
     def __getitem__(self, k):
-        if not 0<=k<n:
+        """Return element at index k."""
+        if not 0<=k<self._n:
             raise IndexError('Invalid index')
         return self._A[k]
 
     def append(self, obj):
+        """Add object to the end of the array"""
         if self._n == self._capacity:
             self._resize(2 * self._capacity)
         self._A[self._n] = obj
         self._n += 1
 
     def _resize(self, c):
+        """Resize internal array to capacity c."""
         B = self._make_array(c)
         for k in range(self._n):
             B[k] = self._A[k]
@@ -30,10 +36,11 @@ class DynamicArray():
         self._capacity = c
 
     def _make_array(self, c):
+        """Return new array with capacity c."""
         return (c * ctypes.py_object)()
     
     def insert(self, index, value):
-        #assumption 0<=index<n
+        """Inserting value at index shifting subsequent values rightwards."""
         if self._n == self._capacity:
             self._resize(2* self.capacity)
         for j in range(self._n, index, -1):
@@ -42,6 +49,7 @@ class DynamicArray():
         self._n += 1
 
     def remove(self, value):
+        """Remove first occurance of value. Raise error if value not fount"""
         for k in range(self._n):
             if self._A[k] == value:
                 for j in range(k, self._n-1):
@@ -52,7 +60,7 @@ class DynamicArray():
         raise ValueError('value not found')
     
     def extend(self, other):
-        """Append other list to our list"""
+        """Append other list to our list."""
         for i in other:
             self._A.append(i)
 
